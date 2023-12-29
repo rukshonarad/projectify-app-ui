1f8a91e2d1da8daa3d4a9c44256d5f6ce09b5035
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-28 22:49:13 -0500

    Create initializer function

--- src/design-system/utils/utils.tsx
+++ src/design-system/utils/utils.tsx
@@ -4,3 +4,9 @@ export const trimWhiteSpaces = (str: string) => {
         .filter((item) => item !== "")
         .join(" ");
 };
+
+export const getNameInitials = (name: string): string => {
+    const words = name.split(" ");
+    const initials = words.map((word) => word.charAt(0).toUpperCase()).join("");
+    return initials;
+};
