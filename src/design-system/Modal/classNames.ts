225edb966039d4a94307b03333341e32d3847204
Rukshona Rad <rukshona.rad@gmail.com>
2024-01-02 19:13:54 -0500

    Add stories default page

--- src/design-system/Modal/classNames.ts
+++
@@ -1,4 +0,0 @@
-export const positionClassNames = {
-    center: "modal-overlay--center",
-    right: "modal-overlay--right"
-};
--- src/design-system/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./Typography";
-export * from "./Button";
-export * from "./Input";
-export * from "./Label";
-export * from "./Modal";
-export * from "./Logo";
-
-export * from "./utils";
