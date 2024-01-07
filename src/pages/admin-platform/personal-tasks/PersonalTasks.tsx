6fef780d14959db025539c9a34a542921ae85ae6
Rukshona Rad <rukshona.rad@gmail.com>
2024-01-07 13:18:32 -0500

    Implement admin platform feature

---
+++ src/pages/admin-platform/personal-tasks/PersonalTasks.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { Input, Modal, Typography, Button } from "../../../design-system";
+import { NoDataPlaceholder } from "../../components";
+import noTask from "../../../assets/images/task.svg";
+
+const PageBase = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+`;
+
+const CreateTaskModalTitle = styled(Typography)`
+    margin-bottom: var(--space-24);
+`;
+
+const Inputs = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: var(--space-16);
+    margin-bottom: var(--space-24);
+`;
+
+const Buttons = styled.div`
+    display: flex;
+    gap: var(--space-10);
+`;
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState<string[]>([]);
+    const [showCreateTaskModal, setShowCreateTaskModal] =
+        useState<boolean>(false);
+
+    return (
+        <PageBase>
+            {!tasks.length ? (
+                <NoDataPlaceholder
+                    illustrationUrl={noTask}
+                    text="You don’t have any tasks yet!"
+                    buttonText="Add a Task"
+                    buttonAction={() => setShowCreateTaskModal(true)}
+                />
+            ) : (
+                <h1>Tasks</h1>
+            )}
+
+            <Modal show={showCreateTaskModal} position="center">
+                <CreateTaskModalTitle variant="paragraphLG" weight="medium">
+                    New Task
+                </CreateTaskModalTitle>
+                <Inputs>
+                    <Input
+                        placeholder="Task Name"
+                        value=""
+                        onChange={() => {}}
+                        shape="rounded"
+                        size="lg"
+                    />
+                    <Input
+                        type="textarea"
+                        placeholder="Task Description"
+                        value=""
+                        onChange={() => {}}
+                        shape="rounded"
+                        size="lg"
+                    />
+                </Inputs>
+                <Buttons>
+                    <Button
+                        color="secondary"
+                        size="lg"
+                        shape="rounded"
+                        variant="outlined"
+                        fullWidth
+                        onClick={() => setShowCreateTaskModal(false)}
+                    >
+                        Cancel
+                    </Button>
+                    <Button size="lg" shape="rounded" color="primary" fullWidth>
+                        Save
+                    </Button>
+                </Buttons>
+            </Modal>
+        </PageBase>
+    );
+};
+
+export { Tasks as AdminTasks };
---
+++ src/pages/admin-platform/projects/Projects.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { Input, Modal, Typography, Button } from "../../../design-system";
+import { NoDataPlaceholder } from "../../components";
+import noProject from "../../../assets/images/project.svg";
+
+const PageBase = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+`;
+
+const CreateProjectModalTitle = styled(Typography)`
+    margin-bottom: var(--space-24);
+`;
+
+const Inputs = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: var(--space-16);
+    margin-bottom: var(--space-24);
+`;
+
+const Buttons = styled.div`
+    display: flex;
+    gap: var(--space-10);
+`;
+
+const Projects = () => {
+    const [projects, setProject] = useState<string[]>([]);
+    const [showCreateProjectModal, setShowCreateProjectModal] =
+        useState<boolean>(false);
+
+    return (
+        <PageBase>
+            {!projects.length ? (
+                <NoDataPlaceholder
+                    illustrationUrl={noProject}
+                    text="You don’t have any projects yet!"
+                    buttonText="Add a Project"
+                    buttonAction={() => setShowCreateProjectModal(true)}
+                />
+            ) : (
+                <h1>Projects</h1>
+            )}
+
+            <Modal show={showCreateProjectModal} position="center">
+                <CreateProjectModalTitle variant="paragraphLG" weight="medium">
+                    New Project
+                </CreateProjectModalTitle>
+                <Inputs>
+                    <Input
+                        placeholder="Project Name"
+                        value=""
+                        onChange={() => {}}
+                        shape="rounded"
+                        size="lg"
+                    />
+                    <Input
+                        type="textarea"
+                        placeholder="Project Description"
+                        value=""
+                        onChange={() => {}}
+                        shape="rounded"
+                        size="lg"
+                    />
+                </Inputs>
+                <Buttons>
+                    <Button
+                        color="secondary"
+                        size="lg"
+                        shape="rounded"
+                        variant="outlined"
+                        fullWidth
+                        onClick={() => setShowCreateProjectModal(false)}
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        size="lg"
+                        shape="rounded"
+                        color="primary"
+                        fullWidth={true}
+                    >
+                        Save
+                    </Button>
+                </Buttons>
+            </Modal>
+        </PageBase>
+    );
+};
+
+export { Projects as AdminProjects };
