eab4a3c3deafb9996e3a0da756f6db6e4c19a324
Rukshona Rad <rukshona.rad@gmail.com>
2024-01-16 22:55:08 -0500

    Create context fpr admin

---
+++ src/context/AppContext.tsx
@@ -0,0 +1,32 @@
+import React, { useState, createContext, useReducer } from "react";
+import {
+    GlobalState,
+    initialState,
+    userReducer,
+    InitUserAction,
+    ActionType
+} from "../store";
+
+type AppContextType = {
+    state: GlobalState;
+    dispatch(action: ActionType): void;
+};
+
+export const AppContext = createContext<AppContextType>({
+    state: initialState,
+    dispatch: () => {}
+});
+
+export const AppProvider: React.FC<{ children: React.ReactNode }> = ({
+    children
+}) => {
+    const [state, dispatch] = useReducer<
+        React.Reducer<GlobalState, ActionType>
+    >(userReducer, initialState);
+    const value = {
+        state,
+        dispatch
+    };
+
+    return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
