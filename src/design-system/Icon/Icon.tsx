af2b3d1d9233d8f3588bcff1c23ecb67ca987c86
Rukshona Rad <rukshona.rad@gmail.com>
2024-01-28 13:05:32 -0500

    Add types to icon

--- src/design-system/Icon/Icon.tsx
+++ src/design-system/Icon/Icon.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import sprite from "./sprite.svg";
+import { IconProps } from "./types";
 
-type IconProps = {
-    iconName: string;
-    className?: string;
-    onClick?: () => void;
-};
-const Icon: React.FC<IconProps> = ({ iconName, className, onClick }) => {
+const Icon: React.FC<IconProps> = ({ iconName, className }) => {
     return (
-        <svg
-            height="2.4rem"
-            width="2.4rem"
-            className={className || ""}
-            onClick={onClick}
-        >
+        <svg height="2.4rem" width="2.4rem" className={className || ""}>
             <use xlinkHref={`${sprite}#${iconName}`} />
         </svg>
     );
