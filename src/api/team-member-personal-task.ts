efca657d369cef984b48e21ed8fe65c654cdd8a0
Rukshona Rad <rukshona.rad@gmail.com>
2024-01-30 19:58:39 -0500

    Hit teamMemberPersonalTask Api

---
+++ src/api/team-member-personal-task.ts
@@ -0,0 +1,111 @@
+import { Task } from "../types";
+
+type TaskCreateInput = Omit<Task, "id" | "status">;
+type TaskUpdateInput = Omit<Task, "id">;
+
+interface GetAllTasksResponse {
+    data: {
+        tasks: Task[] | [];
+    };
+}
+
+interface TaskCreateResponse {
+    data: Task;
+}
+
+class TeamMemberPersonalTasks {
+    url: string;
+    constructor() {
+        this.url = `${
+            process.env.NODE_ENV === "development"
+                ? process.env.REACT_APP_PROJECTIFY_API_URL_LOCAL
+                : process.env.REACT_APP_PROJECTIFY_API_URL
+        }/team-members/me`;
+    }
+
+    async createTask(input: TaskCreateInput): Promise<TaskCreateResponse> {
+        try {
+            const rawAuthToken = localStorage.getItem("authToken");
+            const authToken = rawAuthToken ? JSON.parse(rawAuthToken) : "";
+
+            const response = await fetch(`${this.url}/tasks`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    authorization: `Bearer ${authToken}`
+                },
+                body: JSON.stringify(input)
+            });
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+            return response.json();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getTasks(): Promise<{ data: GetAllTasksResponse }> {
+        try {
+            const rawAuthToken = localStorage.getItem("authToken");
+            const authToken = rawAuthToken ? JSON.parse(rawAuthToken) : "";
+            const response = await fetch(`${this.url}/tasks`, {
+                headers: {
+                    authorization: `Bearer ${authToken}`
+                }
+            });
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+
+            return response.json();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteTask(taskId: string) {
+        try {
+            const rawAuthToken = localStorage.getItem("authToken");
+            const authToken = rawAuthToken ? JSON.parse(rawAuthToken) : "";
+
+            const response = await fetch(`${this.url}/tasks/${taskId}/delete`, {
+                method: "PATCH",
+                headers: {
+                    authorization: `Bearer ${authToken}`
+                }
+            });
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateTask(taskId: string, input: TaskUpdateInput) {
+        try {
+            const rawAuthToken = localStorage.getItem("authToken");
+            const authToken = rawAuthToken ? JSON.parse(rawAuthToken) : "";
+
+            const response = await fetch(`${this.url}/tasks/${taskId}`, {
+                method: "PATCH",
+                headers: {
+                    authorization: `Bearer ${authToken}`
+                },
+                body: JSON.stringify(input)
+            });
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export const teamMemberPersonalTasks = new TeamMemberPersonalTasks();
