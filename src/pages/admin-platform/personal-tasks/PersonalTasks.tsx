8e8397c8760e9660abf875d47210b756098bafe4
Rukshona Rad <rukshona.rad@gmail.com>
2024-01-31 23:25:51 -0500

    Create personal task and update datePicker for admin

--- src/pages/admin-platform/personal-tasks/PersonalTasks.tsx
+++ src/pages/admin-platform/personal-tasks/PersonalTasks.tsx
@@ -1,18 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Input, Modal, Typography, Button } from "../../../design-system";
-import { NoDataPlaceholder } from "../../components";
-
+import {
+    Input,
+    Modal,
+    Typography,
+    Button,
+    DatePickerV1
+} from "../../../design-system";
+import { NoDataPlaceholder, TaskCard, TaskCardProps } from "../../components";
 import noTask from "../../../assets/illustrations/task.svg";
-import { adminPersonalTasks } from "../../../api";
-import toast from "react-hot-toast";
-const PageBase = styled.div`
+import { adminPersonalTasks as adminPersonalTasksService } from "../../../api";
+
+import { useStore } from "../../../hooks";
+import { Actions, PopulateTasksAction } from "../../../store";
+import { groupTasksByStatus } from "../../../utils";
+
+const PageBase = styled.main`
     position: relative;
     width: 100%;
     height: 100%;
 `;
 
-const CreateProjectModalTitle = styled(Typography)`
+const CreateTaskModalTitle = styled(Typography)`
     margin-bottom: var(--space-24);
 `;
 
@@ -28,125 +37,160 @@ const Buttons = styled.div`
     gap: var(--space-10);
 `;
 
-const AdminPersonalTasks = () => {
-    const [personalTasks, setPersonalTasks] = useState<string[]>([]);
+const PageContent = styled.section`
+    width: 80%;
+    margin: 0 auto;
+`;
+
+const PageTitle = styled(Typography)`
+    margin-bottom: var(--space-36);
+`;
+
+const TasksColumns = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: var(--space-30);
+    height: 100%;
+`;
+
+const TasksColumn = styled.div`
+    padding: 2.2rem 1rem 1rem 1rem;
+    background-color: var(--jaguar-25);
+    border-radius: var(--border-radius-20);
+    border: 0.15rem solid var(--jaguar-100);
+`;
+
+const TasksColumnTitle = styled(Typography)`
+    margin-bottom: var(--space-16);
+    color: var(--jaguar-500);
+
+    span {
+        color: var(--jaguar-900);
+    }
+`;
+
+const Tasks = () => {
+    const [taskDue, setTaskDue] = useState<Date>();
+    const [taskTitle, setTaskTitle] = useState<string>("");
+    const [taskDescription, setTaskDescription] = useState<string>("");
+    const [isTasksFetching, setIsTasksFetching] = useState(true);
+    const {
+        state: { adminPersonalTasks },
+        dispatch
+    } = useStore();
+
     const [showCreateTaskModal, setShowCreateTaskModal] =
         useState<boolean>(false);
 
-    const [title, setTitle] = useState<string>("");
-    const [due, setDue] = useState<string>("");
-    const [description, setDescription] = useState<string>("");
-
-    const [isFormSubmitting, setIsFormSubmitting] = useState<boolean>(false);
-
-    const isFormSubmittable = title && due && description;
-
-    const handleOnChangeTitle = (value: string) => {
-        setTitle(value);
-    };
-
-    const handleOnChangeDue = (value: string) => {
-        setDue(value);
-    };
-    const handleOnChangeDescription = (value: string) => {
-        setDescription(value);
-    };
-    const createPersonalTask = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        try {
-            setIsFormSubmitting(true);
-
-            const response = await adminPersonalTasks.createTask({
-                title,
-                description,
-                due
+    useEffect(() => {
+        adminPersonalTasksService
+            .getTasks()
+            .then((data) => {
+                setIsTasksFetching(false);
+                const action: PopulateTasksAction = {
+                    type: Actions.POPULATE_TASKS,
+                    payload: data.data.tasks
+                };
+                dispatch(action);
+            })
+            .catch((error) => {
+                setIsTasksFetching(false);
+                console.log(error);
             });
+    }, []);
 
-            setIsFormSubmitting(false);
-            setTitle("");
-            setDue("");
-            setShowCreateTaskModal(false);
+    if (isTasksFetching) {
+        return null;
+    }
 
-            // toast.success();
-        } catch (error) {
-            if (error instanceof Error) {
-                setIsFormSubmitting(false);
-
-                toast.error(error.message);
-            }
-        }
-    };
+    const groupedTasks = groupTasksByStatus(adminPersonalTasks);
 
     return (
         <PageBase>
-            {!personalTasks.length ? (
+            {!adminPersonalTasks.length ? (
                 <NoDataPlaceholder
                     illustrationUrl={noTask}
-                    text="You don’t have any personal tasks yet!"
+                    text="You don’t have any tasks yet!"
                     buttonText="Add a Task"
                     buttonAction={() => setShowCreateTaskModal(true)}
                 />
             ) : (
-                <h1>personal tasks</h1>
+                <PageContent>
+                    <PageTitle variant="h6" weight="medium">
+                        Personal Tasks
+                    </PageTitle>
+                    <TasksColumns>
+                        {Object.keys(groupedTasks).map((groupName) => {
+                            return (
+                                <TasksColumn key={groupName}>
+                                    <TasksColumnTitle
+                                        variant="paragraphSM"
+                                        weight="semibold"
+                                    >
+                                        {groupName}{" "}
+                                        <span>
+                                            ({groupedTasks[groupName].length})
+                                        </span>
+                                    </TasksColumnTitle>
+
+                                    {groupedTasks[groupName].map((task) => {
+                                        return <TaskCard task={task} />;
+                                    })}
+                                </TasksColumn>
+                            );
+                        })}
+                    </TasksColumns>
+                </PageContent>
             )}
 
-            <form onSubmit={createPersonalTask}>
-                <Modal show={showCreateTaskModal} position="center">
-                    <CreateProjectModalTitle
-                        variant="paragraphLG"
-                        weight="medium"
+            <Modal show={showCreateTaskModal} position="center">
+                <CreateTaskModalTitle variant="paragraphLG" weight="medium">
+                    New Task
+                </CreateTaskModalTitle>
+                <Inputs>
+                    <Input
+                        placeholder="Task Name"
+                        value={taskTitle}
+                        onChange={(value) => setTaskTitle(value)}
+                        shape="rounded"
+                        size="lg"
+                    />
+                    <Input
+                        type="textarea"
+                        placeholder="Task Description"
+                        value={taskDescription}
+                        onChange={(value) => {
+                            setTaskDescription(value);
+                        }}
+                        shape="rounded"
+                        size="lg"
+                    />
+                    <DatePickerV1
+                        inputSize="lg"
+                        shape="rounded"
+                        placeholder="Due Date"
+                        selected={taskDue}
+                        onChange={(date) => setTaskDue(date)}
+                    />
+                </Inputs>
+                <Buttons>
+                    <Button
+                        color="secondary"
+                        size="lg"
+                        shape="rounded"
+                        variant="outlined"
+                        fullWidth
+                        onClick={() => setShowCreateTaskModal(false)}
                     >
-                        New Task
-                    </CreateProjectModalTitle>
-                    <Inputs>
-                        <Input
-                            placeholder="Title"
-                            value={title}
-                            onChange={handleOnChangeTitle}
-                            shape="rounded"
-                            size="lg"
-                        />
-                        <Input
-                            type="textarea"
-                            placeholder="Description"
-                            value={description}
-                            onChange={handleOnChangeDescription}
-                            shape="rounded"
-                            size="lg"
-                        />
-                        <Input
-                            placeholder="Due Date"
-                            value={due}
-                            onChange={handleOnChangeDue}
-                            shape="rounded"
-                            size="lg"
-                        />
-                    </Inputs>
-                    <Buttons>
-                        <Button
-                            color="secondary"
-                            size="lg"
-                            shape="rounded"
-                            variant="outlined"
-                            fullWidth
-                            onClick={() => setShowCreateTaskModal(false)}
-                        >
-                            Cancel
-                        </Button>
-                        <Button
-                            size="lg"
-                            shape="rounded"
-                            color="primary"
-                            fullWidth
-                        >
-                            Save
-                        </Button>
-                    </Buttons>
-                </Modal>
-            </form>
+                        Cancel
+                    </Button>
+                    <Button size="lg" shape="rounded" color="primary" fullWidth>
+                        Save
+                    </Button>
+                </Buttons>
+            </Modal>
         </PageBase>
     );
 };
 
-export { AdminPersonalTasks };
+export { Tasks as AdminPersonalTasks };
