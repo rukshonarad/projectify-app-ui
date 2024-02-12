3f6bc4f912d8142eaddd4af486f9fb91ed9ab7ab
Rukshona Rad <rukshona.rad@gmail.com>
2024-02-12 09:03:40 -0500

    Delete adminTeamMember and joined all logic to teamMember api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
 export * from "./admin";
 export * from "./teamMember";
 export * from "./adminPersonalTasks";
-export * from "./adminTeamMembers";
 export * from "./adminProjects";
 export * from "./adminPersonalTasks";
 export * from "./team-member-personal-task";
--- src/store/reducers/adminTeamMemberReducer.ts
+++ src/store/reducers/adminTeamMemberReducer.ts
@@ -11,16 +11,16 @@ const adminTeamMembersReducer = produce(
     (draft: TeamMemberState, action: ActionType) => {
         switch (action.type) {
             case Actions.ADMIN_ADD_TEAM_MEMBER: {
-                // const payload =
-                //     action.payload as AdminAddTeamMemberAction["payload"];
-                // draft.push(payload);
+                const payload =
+                    action.payload as AdminAddTeamMemberAction["payload"];
+                draft.push(payload);
                 return draft;
             }
-            // case Actions.ADMIN_POPULATE_TEAM_MEMBERS: {
-            //     const payload =
-            //     //     action.payload as AdminPopulateTeamMembersAction["payload"];
-            //     // return payload;
-            // }
+            case Actions.ADMIN_POPULATE_TEAM_MEMBERS: {
+                const payload =
+                    action.payload as AdminPopulateTeamMembersAction["payload"];
+                return payload;
+            }
             default:
                 return draft;
         }
