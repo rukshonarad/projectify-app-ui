d6544cf9cae49b4d5bc2da9ca4057e4f37067ed5
Rukshona Rad <rukshona.rad@gmail.com>
2024-02-21 12:15:19 -0500

    Implement search by teamMember test

--- src/pages/admin-platform/team-members/TeamMemberFilters.tsx
+++ src/pages/admin-platform/team-members/TeamMemberFilters.tsx
@@ -1,24 +1,31 @@
 import styled from "styled-components";
-import { Option, Select } from "../../../design-system";
+import { Input, Option, Select } from "../../../design-system";
 import React from "react";
 
 type TeamMemberFiltersProps = {
     selectedStatus: string;
     setSelectedStatus: (option: Option) => void;
+    searchText: string;
+    setSearchText: (value: string) => void;
 };
 
 const FiltersBase = styled.section`
     display: grid;
-    grid-template-columns: 20rem 1fr 20rem;
+    grid-template-columns: 20rem 20rem 1fr 20rem;
     align-items: center;
+    gap: var(--space-24);
     margin-bottom: var(--space-20);
 
-    .filter-by-project {
+    .filter-by-status {
         grid-column: 1 / 2;
     }
 
-    .filter-by-status {
-        grid-column: 3 / 4;
+    .search {
+        grid-column: 2 / 3;
+    }
+
+    .filter-by-project {
+        grid-column: 4 / 5;
     }
 `;
 const statusOptions = [
@@ -29,19 +36,12 @@ const statusOptions = [
 ];
 const TeamMemberFilters: React.FC<TeamMemberFiltersProps> = ({
     selectedStatus,
-    setSelectedStatus
+    setSelectedStatus,
+    searchText,
+    setSearchText
 }) => {
     return (
         <FiltersBase>
-            <Select
-                value=""
-                onSelect={() => {}}
-                options={[]}
-                shape="rounded"
-                size="md"
-                headerPlaceholder="By Project"
-                className="filter-by-project"
-            />
             <Select
                 value={selectedStatus}
                 onSelect={setSelectedStatus}
@@ -50,6 +50,23 @@ const TeamMemberFilters: React.FC<TeamMemberFiltersProps> = ({
                 size="md"
                 headerPlaceholder="By Status"
                 className="filter-by-status"
+            />
+            <Input
+                value={searchText}
+                onChange={setSearchText}
+                placeholder="Search..."
+                shape="rounded"
+                size="md"
+                className="search"
+            />
+            <Select
+                value=""
+                onSelect={() => {}}
+                options={[]}
+                shape="rounded"
+                size="md"
+                headerPlaceholder="By Project"
+                className="filter-by-project"
             />
         </FiltersBase>
     );
