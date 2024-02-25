2def99c7f07c09a10c10bfce755cdd2b830a2ad0
Rukshona Rad <rukshona.rad@gmail.com>
2024-02-25 10:44:34 -0500

    Change due date format

--- src/pages/admin-platform/personal-tasks/CreateTaskModal.tsx
+++ src/pages/admin-platform/personal-tasks/CreateTaskModal.tsx
@@ -12,7 +12,7 @@ import {
 import { TaskCreateInput, adminTasksService } from "../../../api";
 import { useStore } from "../../../hooks";
 import { Actions, AddTaskAction } from "../../../store";
-
+import { toIso8601 } from "../../../utils";
 type CreateTaskModalProps = {
     show: boolean;
     closeModal: () => void;
@@ -50,7 +50,7 @@ const CreateTaskModal: React.FC<CreateTaskModalProps> = ({
         const input: TaskCreateInput = {
             title: taskTitle,
             description: taskDescription,
-            due: taskDue!
+            due: toIso8601(taskDue!)
         };
 
         adminTasksService
